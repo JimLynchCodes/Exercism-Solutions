02c5a4e28d5bd1d918e0aedf987df10e604eb07b
JimTheMan <mrdotjim@gmail.com>
2020-02-09 21:43:28 -0500

    starting c# and f# tracks\!

---
+++ ballerina/hello-world/hello_world.bal
@@ -0,0 +1,7 @@
+import ballerina/io;
+
+// Simply return the string 'Hello, World!'
+
+public function main() {
+   io:println("replace me");
+}
---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class TwoFer
+{
+    public static string Speak(string name = "you")
+    {
+        return $"One for {name}, one for me.";
+    }
+}
