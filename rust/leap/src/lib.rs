4996749a4f54a4f1b14a70a705b5d74a68077153
JimTheMan <mrdotjim@gmail.com>
2020-02-18 00:11:51 -0500

    rust and f# leap

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,16 +1,3 @@
 pub fn is_leap_year(year: u64) -> bool {
-
-    if year % 400 == 0 {
-        return true;
-    }
-
-    if year % 100 == 0 {
-        return false;
-    }
-
-    if year % 4 == 0 {
-        return true;
-    }
-
-    return false;
+    year % 400 == 0 || year % 4 == 0 && year % 100 != 0
 }
