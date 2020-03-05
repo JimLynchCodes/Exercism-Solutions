95204ba89acbd59c5a9b42a381afe552f745d022
JimLynchCodes <mrdotjim@gmail.com>
2020-03-05 15:18:06 -0500

    tight lil' python soln's\!

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,15 +1,11 @@
+
+DROP_TYPES = [
+    (3, 'Pling'),
+    (5, 'Plang'),
+    (7, 'Plong'),
+]
+
+
 def convert(n):
-
-    result = ''
-    
-    dropTypes = [
-        (3, 'Pling'),
-        (5, 'Plang'),
-        (7, 'Plong'),
-    ]
-
-    for dropType in dropTypes:
-        if (n % dropType[0] == 0):
-            result = result + dropType[1]
-
-    return result or str(n)
+    result = [sound for factor, sound in DROP_TYPES if n % factor == 0]
+    return ''.join(result) or str(n)
