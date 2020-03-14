6f01c534d8d7c7acaea314c24ab93c72ed0e02f0
JimLynchCodes <mrdotjim@gmail.com>
2020-03-14 12:43:58 -0400

    typescript RLE solution

--- typescript/run-length-encoding/run-length-encoding.ts
+++ typescript/run-length-encoding/run-length-encoding.ts
@@ -1,9 +1,32 @@
 export default {
     encode: (s: string): string => {
 
-        return s
+        let current
+        let count = 1
+        let previous = s[0]
+        let encoded = ''
+
+        for (let i = 1; i <= s.length; i++) {
+            current = s[i]
+            if (current === previous)
+                count++
+            else {
+                encoded += (count == 1 ? '' : count) + previous
+                count = 1
+                previous = current
+            }
+        }
+
+        return encoded
     },
     decode: (s: string): string => {
+
+        const numbers = s.match(/[0-9]+/g)
+
+        numbers?.forEach((n: string) => {
+            const indexOfNum = s.indexOf(n)
+            s = s.replace(n, s[indexOfNum + n.length].repeat(parseInt(n) - 1))
+        })
 
         return s
     }
