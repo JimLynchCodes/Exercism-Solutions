adc162f571304b069feb6b3c491a6260a52ae253
JimLynchCodes <mrdotjim@gmail.com>
2020-03-29 12:16:34 -0400

    improved pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -4,8 +4,8 @@ const ALPHABET_LENGTH = 26
 export const isPangram = (str) => {
 
   const lowercase = str.toLowerCase()
-  const lettersOnly = lowercase.match(/[a-zA-Z]+/g) || []
-  const uniques = new Set(lettersOnly.join(''))
+  const lettersOnly = lowercase.match(/[a-z]/g)
+  const uniques = new Set(lettersOnly)
 
   return uniques.size === ALPHABET_LENGTH
 }
