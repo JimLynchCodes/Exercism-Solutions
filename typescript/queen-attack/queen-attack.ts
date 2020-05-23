aadc86ffd30485ed1236d5b85d4a9911ebcdbcbc
JimLynchCodes <mrdotjim@gmail.com>
2020-05-23 19:50:40 -0400

    queens attack ts soln

---
+++ typescript/queen-attack/queen-attack.ts
@@ -0,0 +1,110 @@
+
+const ROWS_IN_CHESSBOARD = 8
+const COLUMNS_IN_CHESSBOARD = 8
+
+enum DiagonalDirection {
+    UpAndToTheLeft,
+    UpAndToTheRight,
+    DownAndToTheLeft,
+    DownAndToTheRight,
+}
+
+export default class QueenAttack {
+
+    white: number[]
+    black: number[]
+
+    board: string
+
+    canAttackEachOther: boolean
+
+    constructor(positions: { white: number[], black: number[] }) {
+
+        if (positions.white[0] === positions.black[0] &&
+            positions.white[1] === positions.black[1])
+            throw new Error('Queens cannot share the same space')
+
+        this.white = positions.white
+        this.black = positions.black
+
+        this.board = this.createBoard(this.white, this.black)
+
+        this.canAttackEachOther = this.checkIfQueensCanAttackEachOther(this.white, this.black)
+    }
+
+    toString(): string {
+        return this.board
+    }
+
+    canAttack(): boolean {
+        return this.canAttackEachOther
+    }
+
+    private createBoard(white: number[], black: number[]): string {
+        return [...Array(ROWS_IN_CHESSBOARD).keys()].map((_row, _rowIndex) => {
+
+            return [...Array(COLUMNS_IN_CHESSBOARD).keys()].map((_column, _columnIndex) => {
+
+                if (white[0] === _rowIndex && white[1] === _columnIndex)
+                    return 'W'
+
+                if (black[0] === _rowIndex && black[1] === _columnIndex)
+                    return 'B'
+
+                return '_'
+
+            }).join(' ')
+
+        }).join('\n') + '\n'
+    }
+
+    private checkIfQueensCanAttackEachOther(_white: number[], _black: number[]): boolean {
+
+        // Queens in the same row or column can attack each other.
+        if (_white[0] === _black[0] || _white[1] === _black[1])
+            return true
+
+        return this.canQueensAttackEachOtherDiagonally(_white, _black)
+
+    }
+
+    private canQueensAttackEachOtherDiagonally(_white: number[], _black: number[]): boolean {
+
+        return this.canQueensAttackInDirection(_white, _black, DiagonalDirection.UpAndToTheLeft) ||
+            this.canQueensAttackInDirection(_white, _black, DiagonalDirection.UpAndToTheRight) ||
+            this.canQueensAttackInDirection(_white, _black, DiagonalDirection.DownAndToTheLeft) ||
+            this.canQueensAttackInDirection(_white, _black, DiagonalDirection.DownAndToTheRight)
+
+    }
+
+    private canQueensAttackInDirection(attackingQueen: number[], targetQueen: number[], direction: DiagonalDirection): boolean {
+
+        if (this.occupySameSquare(attackingQueen, targetQueen))
+            return true
+
+        if (direction === DiagonalDirection.UpAndToTheLeft)
+            if (attackingQueen[0] > 0 && attackingQueen[1] > 0)
+                return this.canQueensAttackInDirection([attackingQueen[0] - 1, attackingQueen[1] - 1], targetQueen, direction)
+
+        if (direction === DiagonalDirection.UpAndToTheRight)
+            if (attackingQueen[0] > 0 && attackingQueen[1] < (COLUMNS_IN_CHESSBOARD - 1))
+                return this.canQueensAttackInDirection([attackingQueen[0] - 1, attackingQueen[1] + 1], targetQueen, direction)
+
+
+        if (direction === DiagonalDirection.DownAndToTheLeft)
+            if (attackingQueen[0] < (ROWS_IN_CHESSBOARD - 1) && attackingQueen[1] > 0)
+                return this.canQueensAttackInDirection([attackingQueen[0] + 1, attackingQueen[1] - 1], targetQueen, direction)
+
+        if (direction === DiagonalDirection.DownAndToTheRight)
+            if (attackingQueen[0] < (ROWS_IN_CHESSBOARD - 1) && attackingQueen[1] < (COLUMNS_IN_CHESSBOARD - 1))
+                return this.canQueensAttackInDirection([attackingQueen[0] + 1, attackingQueen[1] + 1], targetQueen, direction)
+
+        return false
+
+    }
+
+    private occupySameSquare(queenA: number[], queenB: number[]): boolean {
+        return queenA[0] === queenB[0] && queenA[1] === queenB[1]
+    }
+
+}
