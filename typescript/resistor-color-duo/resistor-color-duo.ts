7737e64f1d793bad70c87f5fb746e1681ebca4dc
JimLynchCodes <mrdotjim@gmail.com>
2020-05-31 20:51:03 -0400

    new resister-color-duo typescript soln

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,19 +1,25 @@
 export class ResistorColor {
-  readonly Colors: string[] = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
-  readonly MINIMUM_ACCEPTABLE_COLORS = 2;
+  
+  private readonly Colors: string[] = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+  
+  private readonly MINIMUM_ACCEPTABLE_COLORS = 2;
 
   constructor(private colors: string[]) {
-    if (colors.length < this.MINIMUM_ACCEPTABLE_COLORS) throw "At least two colors need to be present"
-    this.colors = colors.slice(0, 2);
+    
+    if (colors.length < this.MINIMUM_ACCEPTABLE_COLORS)
+      throw "At least two colors need to be present";
+
   }
 
   value = (): number => {
-
-    const result = this.colors.reduce(
-      (accumulator: string, current: string) =>
-        `${accumulator}${this.Colors.indexOf(current)}`,
-      ""
-    );
+    
+    const result = this.colors
+      .slice(0, this.MINIMUM_ACCEPTABLE_COLORS)
+      .reduce(
+        (accumulator: string, current: string) =>
+          `${accumulator}${this.Colors.indexOf(current)}`,
+          ''
+      );
 
     return parseInt(result);
 
