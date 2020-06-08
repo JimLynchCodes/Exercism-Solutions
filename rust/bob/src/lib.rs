6a9e2a5e16af3d5f7abbfcc608b872280112678b
JimLynchCodes <mrdotjim@gmail.com>
2020-06-07 21:31:42 -0400

    amke default response const

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,10 +1,11 @@
+const DEFAULT_RESPONSE: &str = "Whatever.";
 const RESPONSE_FOR_QUESTION: &str = "Sure.";
 const RESPONSE_FOR_SHOUTING: &str = "Whoa, chill out!";
 const RESPONSE_FOR_SHOUTED_QUESTION: &str = "Calm down, I know what I'm doing!";
 const RESPONSE_FOR_NOTHING: &str = "Fine. Be that way!";
 
 pub fn reply(_message: &str) -> &str {
-    let mut response = "Whatever.";
+    let mut response = DEFAULT_RESPONSE;
 
     let is_shouted = _message.to_uppercase() == _message;
     let is_question = _message.trim().ends_with('?');
