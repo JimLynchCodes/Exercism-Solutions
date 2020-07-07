e98b36fa8ba3f053c7e982861220402e5b413e57
JimLynchCodes <mrdotjim@gmail.com>
2020-07-06 20:23:53 -0400

    rust grains soln

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,24 +1,15 @@
-const SQUARES_IN_CHESSBOARD: u32 = 64;
+const MULTIPLIER_PER_SQUARE: u64 = 2;
+const TOTAL_NUMBER_OF_SQUARES: u32 = 64;
 
 pub fn square(s: u32) -> u64 {
-    // unimplemented!("grains of rice on square {}", s);
-
-    if s < 1 || s > SQUARES_IN_CHESSBOARD {
-        panic!("Square must be between 1 and 64")
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64");
     }
 
-    2u64.pow(s - 1)
+    let base: u64 = MULTIPLIER_PER_SQUARE;
+    base.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    let total_squares = 0..SQUARES_IN_CHESSBOARD;
-
-    return total_squares.fold(0, |mut sum, val| {
-        let grains_of_sand_for_square = 2u64.pow(val);
-
-        sum += grains_of_sand_for_square;
-
-        sum
-    });
-
+    (1..=TOTAL_NUMBER_OF_SQUARES).map(square).sum()
 }
