6af7177379f9f83ecedc23fb6b9c0b89f1f088d3
JimLynchCodes <mrdotjim@gmail.com>
2020-08-08 13:00:19 -0400

    rust bob solution using succinct match

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -4,29 +4,16 @@ const RESPONSE_FOR_SHOUTING: &str = "Whoa, chill out!";
 const RESPONSE_FOR_SHOUTED_QUESTION: &str = "Calm down, I know what I'm doing!";
 const RESPONSE_FOR_NOTHING: &str = "Fine. Be that way!";
 
-pub fn reply(_message: &str) -> &str {
-    let mut response = DEFAULT_RESPONSE;
+pub fn reply(message: &str) -> &str {
+    let message = message.trim_end();
+    let is_question = message.trim_end().ends_with('?');
+    let is_shouted = message.to_uppercase() == message && message.contains(char::is_alphabetic);
 
-    let is_shouted = _message.to_uppercase() == _message;
-    let is_question = _message.trim().ends_with('?');
-    let is_nothing = _message.trim().is_empty();
-    let contains_letters = _message.find(char::is_alphabetic).is_some();
-
-    if is_question {
-        response = RESPONSE_FOR_QUESTION;
+    match message {
+        _m if message.is_empty() => RESPONSE_FOR_NOTHING,
+        _m if is_question && is_shouted => RESPONSE_FOR_SHOUTED_QUESTION,
+        _m if is_question => RESPONSE_FOR_QUESTION,
+        _m if is_shouted => RESPONSE_FOR_SHOUTING,
+        _ => DEFAULT_RESPONSE,
     }
-
-    if is_shouted && contains_letters {
-        response = RESPONSE_FOR_SHOUTING;
-    }
-
-    if is_shouted && is_question && contains_letters {
-        response = RESPONSE_FOR_SHOUTED_QUESTION;
-    }
-
-    if is_nothing {
-        response = RESPONSE_FOR_NOTHING;
-    }
-
-    response
 }
--- rust/bob/tests/bob.rs
+++ rust/bob/tests/bob.rs
@@ -7,12 +7,10 @@ fn test_stating_something() {
     process_response_case("Tom-ay-to, tom-aaaah-to.", "Whatever.");
 }
 
-
 #[test]
 fn test_ending_with_whitespace() {
     process_response_case("Okay if like my  spacebar  quite a bit?   ", "Sure.");
 }
-
 
 #[test]
 
@@ -21,7 +19,6 @@ fn test_shouting_numbers() {
     process_response_case("1, 2, 3 GO!", "Whoa, chill out!");
 }
 
-
 #[test]
 
 /// other whitespace
@@ -29,14 +26,15 @@ fn test_other_whitespace() {
     process_response_case("\r\r 	", "Fine. Be that way!");
 }
 
-
 #[test]
 
 /// shouting with special characters
 fn test_shouting_with_special_characters() {
-    process_response_case("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!", "Whoa, chill out!");
+    process_response_case(
+        "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!",
+        "Whoa, chill out!",
+    );
 }
-
 
 #[test]
 
@@ -45,7 +43,6 @@ fn test_talking_forcefully() {
     process_response_case("Hi there!", "Whatever.");
 }
 
-
 #[test]
 
 /// prattling on
@@ -53,14 +50,12 @@ fn test_prattling_on() {
     process_response_case("Wait! Hang on. Are you going to be OK?", "Sure.");
 }
 
-
 #[test]
 
 /// forceful question
 fn test_forceful_question() {
-   process_response_case("WHAT'S GOING ON?", "Calm down, I know what I'm doing!");
+    process_response_case("WHAT'S GOING ON?", "Calm down, I know what I'm doing!");
 }
-
 
 #[test]
 
@@ -69,14 +64,12 @@ fn test_shouting_with_no_exclamation_mark() {
     process_response_case("I HATE THE DENTIST", "Whoa, chill out!");
 }
 
-
 #[test]
 
 /// asking gibberish
 fn test_asking_gibberish() {
     process_response_case("fffbbcbeab?", "Sure.");
 }
-
 
 #[test]
 
@@ -85,14 +78,12 @@ fn test_question_with_no_letters() {
     process_response_case("4?", "Sure.");
 }
 
-
 #[test]
 
 /// no letters
 fn test_no_letters() {
     process_response_case("1, 2, 3", "Whatever.");
 }
-
 
 #[test]
 
@@ -101,23 +92,26 @@ fn test_statement_containing_question_mark() {
     process_response_case("Ending with ? means a question.", "Whatever.");
 }
 
-
 //NEW
 #[test]
 
 /// multiple line question
 fn test_multiple_line_question() {
-    process_response_case("\rDoes this cryogenic chamber make me look fat?\rNo.", "Whatever.");
+    process_response_case(
+        "\rDoes this cryogenic chamber make me look fat?\rNo.",
+        "Whatever.",
+    );
 }
-
 
 #[test]
 
 /// non-question ending with whitespace
 fn test_nonquestion_ending_with_whitespace() {
-    process_response_case("This is a statement ending with whitespace      ", "Whatever.");
+    process_response_case(
+        "This is a statement ending with whitespace      ",
+        "Whatever.",
+    );
 }
-
 
 #[test]
 #[ignore]
@@ -126,14 +120,12 @@ fn test_shouting() {
     process_response_case("WATCH OUT!", "Whoa, chill out!");
 }
 
-
 #[test]
 #[ignore]
 /// non-letters with question
 fn test_nonletters_with_question() {
     process_response_case(":) ?", "Sure.");
 }
-
 
 #[test]
 #[ignore]
@@ -142,14 +134,12 @@ fn test_shouting_gibberish() {
     process_response_case("FCECDFCAAB", "Whoa, chill out!");
 }
 
-
 #[test]
 #[ignore]
 /// asking a question
 fn test_asking_a_question() {
     process_response_case("Does this cryogenic chamber make me look fat?", "Sure.");
 }
-
 
 #[test]
 #[ignore]
@@ -158,14 +148,12 @@ fn test_asking_a_numeric_question() {
     process_response_case("You are, what, like 15?", "Sure.");
 }
 
-
 #[test]
 #[ignore]
 /// silence
 fn test_silence() {
     process_response_case("", "Fine. Be that way!");
 }
-
 
 #[test]
 #[ignore]
@@ -174,14 +162,15 @@ fn test_starting_with_whitespace() {
     process_response_case("         hmmmmmmm...", "Whatever.");
 }
 
-
 #[test]
 #[ignore]
 /// using acronyms in regular speech
 fn test_using_acronyms_in_regular_speech() {
-    process_response_case("It's OK if you don't want to go work for NASA.", "Whatever.");
+    process_response_case(
+        "It's OK if you don't want to go work for NASA.",
+        "Whatever.",
+    );
 }
-
 
 #[test]
 #[ignore]
@@ -190,11 +179,9 @@ fn test_alternate_silence() {
     process_response_case("										", "Fine. Be that way!");
 }
 
-
 #[test]
 #[ignore]
 /// prolonged silence
 fn test_prolonged_silence() {
     process_response_case("          ", "Fine. Be that way!");
 }
-
