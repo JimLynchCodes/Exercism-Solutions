57100970d3593f226eb208ffd650a590f8c27334
JimLynchCodes <mrdotjim@gmail.com>
2020-08-08 20:40:02 -0400

    rust bob using match

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -5,15 +5,22 @@ const RESPONSE_FOR_SHOUTED_QUESTION: &str = "Calm down, I know what I'm doing!";
 const RESPONSE_FOR_NOTHING: &str = "Fine. Be that way!";
 
 pub fn reply(message: &str) -> &str {
-    let message = message.trim_end();
-    let is_question = message.trim_end().ends_with('?');
-    let is_shouted = message.to_uppercase() == message && message.contains(char::is_alphabetic);
+    let is_empty = message.trim_end().is_empty();
+    let is_yelled = message == message.to_uppercase();
+    let is_question = message.trim_end().ends_with("?");
+    let contains_characters = message.contains(char::is_alphabetic);
 
-    match message {
-        _m if message.is_empty() => RESPONSE_FOR_NOTHING,
-        _m if is_question && is_shouted => RESPONSE_FOR_SHOUTED_QUESTION,
-        _m if is_question => RESPONSE_FOR_QUESTION,
-        _m if is_shouted => RESPONSE_FOR_SHOUTING,
+    match (
+        is_empty,
+        is_question,
+        is_yelled,
+        contains_characters,
+    ) {
+        (true, _, _, _) => RESPONSE_FOR_NOTHING,
+        (_, true, true, true) => RESPONSE_FOR_SHOUTED_QUESTION,
+        (_, false, true, true) => RESPONSE_FOR_SHOUTING,
+        (_, true, _, _) => RESPONSE_FOR_QUESTION,
         _ => DEFAULT_RESPONSE,
     }
+
 }
--- rust/bob/tests/bob.rs
+++ rust/bob/tests/bob.rs
@@ -114,56 +114,55 @@ fn test_nonquestion_ending_with_whitespace() {
 }
 
 #[test]
-#[ignore]
 /// shouting
 fn test_shouting() {
     process_response_case("WATCH OUT!", "Whoa, chill out!");
 }
 
 #[test]
-#[ignore]
+
 /// non-letters with question
 fn test_nonletters_with_question() {
     process_response_case(":) ?", "Sure.");
 }
 
 #[test]
-#[ignore]
+
 /// shouting gibberish
 fn test_shouting_gibberish() {
     process_response_case("FCECDFCAAB", "Whoa, chill out!");
 }
 
 #[test]
-#[ignore]
+
 /// asking a question
 fn test_asking_a_question() {
     process_response_case("Does this cryogenic chamber make me look fat?", "Sure.");
 }
 
 #[test]
-#[ignore]
+
 /// asking a numeric question
 fn test_asking_a_numeric_question() {
     process_response_case("You are, what, like 15?", "Sure.");
 }
 
 #[test]
-#[ignore]
+
 /// silence
 fn test_silence() {
     process_response_case("", "Fine. Be that way!");
 }
 
 #[test]
-#[ignore]
+
 /// starting with whitespace
 fn test_starting_with_whitespace() {
     process_response_case("         hmmmmmmm...", "Whatever.");
 }
 
 #[test]
-#[ignore]
+
 /// using acronyms in regular speech
 fn test_using_acronyms_in_regular_speech() {
     process_response_case(
@@ -173,14 +172,14 @@ fn test_using_acronyms_in_regular_speech() {
 }
 
 #[test]
-#[ignore]
+
 /// alternate silence
 fn test_alternate_silence() {
     process_response_case("										", "Fine. Be that way!");
 }
 
 #[test]
-#[ignore]
+
 /// prolonged silence
 fn test_prolonged_silence() {
     process_response_case("          ", "Fine. Be that way!");
