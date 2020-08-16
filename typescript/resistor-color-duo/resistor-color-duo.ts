c28b3656eb9617003ad8241e3b5f6d20ad1b9065
JimLynchCodes <mrdotjim@gmail.com>
2020-08-16 14:56:45 -0400

    refactor resister-color-duo in ts

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,27 +1,39 @@
+
+enum Color {
+  black = 0,
+  brown = 1,
+  red = 2,
+  orange = 3,
+  yellow = 4,
+  green = 5,
+  blue = 6,
+  violet = 7,
+  grey = 8,
+  white = 9
+}
+
 export class ResistorColor {
-  
-  private readonly Colors: string[] = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
-  
+
   private readonly MINIMUM_ACCEPTABLE_COLORS = 2;
 
   constructor(private colors: string[]) {
-    
+
     if (colors.length < this.MINIMUM_ACCEPTABLE_COLORS)
-      throw "At least two colors need to be present";
+      throw "At least two colors need to be present"
 
   }
 
   value = (): number => {
-    
-    const result = this.colors
+
+    const result = (this.colors as unknown as Color[])
       .slice(0, this.MINIMUM_ACCEPTABLE_COLORS)
       .reduce(
-        (accumulator: string, current: string) =>
-          `${accumulator}${this.Colors.indexOf(current)}`,
-          ''
+        (accumulator: string, color: Color) =>
+          `${accumulator}${Color[color]}`,
+        ''
       );
 
-    return parseInt(result);
+    return parseInt(result)
 
   }
 
