48829f8da1f5b67a47815b348264fd9712590881
JimLynchCodes <mrdotjim@gmail.com>
2020-08-16 15:03:55 -0400

    some more Rust exercises

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,15 +1,8 @@
-const MULTIPLIER_PER_SQUARE: u64 = 2;
-const TOTAL_NUMBER_OF_SQUARES: u32 = 64;
 
 pub fn square(s: u32) -> u64 {
-    if s < 1 || s > 64 {
-        panic!("Square must be between 1 and 64");
-    }
-
-    let base: u64 = MULTIPLIER_PER_SQUARE;
-    base.pow(s - 1)
+    panic!("unimplemented!");
 }
 
 pub fn total() -> u64 {
-    (1..=TOTAL_NUMBER_OF_SQUARES).map(square).sum()
+    panic!("unimplemented!");
 }
