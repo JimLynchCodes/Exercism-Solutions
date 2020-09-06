7b558bb8a940f3fa6da38670a6408fb4a4780e9b
JimLynchCodes <mrdotjim@gmail.com>
2020-09-06 12:16:07 -0400

    new raindrops soln

--- typescript/raindrops/raindrops.ts
+++ typescript/raindrops/raindrops.ts
@@ -1,26 +1,22 @@
 export default class Raindrops {
 
-    convert(_drops: number): string {
+    convert(drops: number): string {
 
         let sound = ''
 
-        if (_drops % 3 === 0) {
+        if (drops % 3 === 0) {
             sound += 'Pling'
         }
 
-        if (_drops % 5 === 0) {
+        if (drops % 5 === 0) {
             sound += 'Plang'
         }
         
-        if (_drops % 7 === 0) {
+        if (drops % 7 === 0) {
             sound += 'Plong'
         }
 
-        if (sound === '') {
-            return _drops.toString();
-        } 
-        
-        return sound
+        return sound || drops.toString()
 
     }
 
