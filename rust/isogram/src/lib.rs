e57e8768ad4100eba906542d1f6bcf341189e5f3
JimLynchCodes <mrdotjim@gmail.com>
2022-02-19 18:55:31 -0500

    rust isogram soln

---
+++ rust/isogram/src/lib.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+pub fn check(candidate: &str) -> bool {
+    let mut letters = HashMap::<char, bool>::new();
+
+    let lowercase_candidate: String = candidate.to_lowercase();
+    let candidate_no_spaces: String = lowercase_candidate.split_whitespace().collect();
+    let candidate_no_dashes: String = candidate_no_spaces.replace("-", "");
+    let vec_of_candidate_chars: Vec<char> = candidate_no_dashes.chars().collect();
+
+    for letter in vec_of_candidate_chars {
+        if let Some(_) = letters.get(&letter) {
+            return false;
+        } else {
+            letters.insert(letter, true);
+        }
+    }
+    true
+}
