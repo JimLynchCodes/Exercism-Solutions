12ec26ecb840ce9d5cd87d2d248e07d0bc1b253b
JimLynchCodes <mrdotjim@gmail.com>
2022-02-19 19:12:13 -0500

    cleaned vec of chars one liner

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -3,16 +3,20 @@ use std::collections::HashMap;
 pub fn check(candidate: &str) -> bool {
     let mut letters = HashMap::<char, bool>::new();
 
-    let lowercase_candidate: String = candidate.to_lowercase();
-    let candidate_no_spaces: String = lowercase_candidate.split_whitespace().collect();
-    let candidate_no_dashes: String = candidate_no_spaces.replace("-", "");
-    let vec_of_candidate_chars: Vec<char> = candidate_no_dashes.chars().collect();
+    let clean_candidate_characters: Vec<char> = candidate
+        .to_lowercase()
+        .split_whitespace()
+        .collect::<String>()
+        .replace("-", "-")
+        .chars()
+        .collect();
 
-    for letter in vec_of_candidate_chars {
-        
+    for letter in clean_candidate_characters {
         match letters.get(&letter) {
-            Some(_) => { return false },
-            None => { letters.insert(letter, true); }
+            Some(_) => return false,
+            None => {
+                letters.insert(letter, true);
+            }
         }
     }
     true
