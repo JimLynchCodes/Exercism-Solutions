2772bd1894e8c31c14da9349d5cf02569a7a5a57
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-10-05 11:29:43 -0400

    lasagna java

---
+++ java/lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,20 @@
+public class Lasagna {
+
+    final int PREP_TIME_PER_LAYER = 2;
+    
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+    
+    public int remainingMinutesInOven(int minCooked) {
+        return expectedMinutesInOven() - minCooked;
+    }
+    
+    public int preparationTimeInMinutes(int layers) {
+        return layers * PREP_TIME_PER_LAYER;
+    }
+    
+    public int totalTimeInMinutes(int layers, int minCooked) {
+        return preparationTimeInMinutes(layers) + minCooked;
+    }
+}
